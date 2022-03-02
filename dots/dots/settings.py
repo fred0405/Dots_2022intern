c1df198d0d730c9dc8b3a7c9ea350d5ac706a214
samLiu <fredliu0405@gmail.com>
2022-03-02 15:25:41 -0500

    add Docker

--- dots/dots/settings.py
+++ dots/dots/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-hm(io3mld73=(b%$r3jufmwq34)10of48#o2@ikoz5et#hiqry
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
